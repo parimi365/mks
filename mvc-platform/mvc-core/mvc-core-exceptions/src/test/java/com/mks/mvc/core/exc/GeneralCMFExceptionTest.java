175f384be707cb1a1f95c33143050578000045b5
karthik.muthyam <karthik.muthyam@cgi.com>
2017-11-13 12:00:55 +0530

    added code changes

--- mvc-platform/mvc-core/mvc-core-exceptions/src/test/java/com/mks/mvc/core/exc/GeneralCMFExceptionTest.java
+++ mvc-platform/mvc-core/mvc-core-exceptions/src/test/java/com/mks/mvc/core/exc/GeneralCMFExceptionTest.java
@@ -1,5 +1,17 @@
 /*******************************************************************************
- * © 2017 CGI Nederland B.V., All rights are reserved.
+* mks @2017 Warangal ,All rights are rederved.Copyright 2017 karthik.muthyam
+* mks @2017 Warangal ,All rights are rederved.
+* mks @2017 Warangal ,All rights are rederved.Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* mks @2017 Warangal ,All rights are rederved.use this file except in compliance with the License.  You may obtain a copy
+* mks @2017 Warangal ,All rights are rederved.of the License at
+* mks @2017 Warangal ,All rights are rederved.
+* mks @2017 Warangal ,All rights are rederved.  http://www.apache.org/licenses/LICENSE-2.0
+* mks @2017 Warangal ,All rights are rederved.
+* mks @2017 Warangal ,All rights are rederved.Unless required by applicable law or agreed to in writing, software
+* mks @2017 Warangal ,All rights are rederved.distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* mks @2017 Warangal ,All rights are rederved.WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+* mks @2017 Warangal ,All rights are rederved.License for the specific language governing permissions and limitations under
+* mks @2017 Warangal ,All rights are rederved.the License.
  ******************************************************************************/
 package com.mks.mvc.core.exc;
 
