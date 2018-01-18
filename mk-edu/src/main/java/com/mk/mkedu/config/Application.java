e71a49545fc356aa7168fab24ad6b4a962b2125e
karthik.muthyam <karthik.muthyam@cgi.com>
2018-01-18 19:02:50 +0530

    added

--- mk-edu/src/main/java/com/mk/mkedu/config/Application.java
+++ mk-edu/src/main/java/com/mk/mkedu/config/Application.java
@@ -1,5 +1,7 @@
 package com.mk.mkedu.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
@@ -11,9 +13,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 public class Application
 {
-
+	private static final Logger logger = LogManager.getLogger(Application.class);
     public static void main(String[] args) throws Throwable
     {
         SpringApplication.run(Application.class, args);
+        logger.info("application class is done server is going to up");
     }
 }
